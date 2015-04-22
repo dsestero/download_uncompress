a1532c69c5d45bec8a5596d81fd61209bb16b268
Dario Sestero <dsestero@gmail.com>
2015-04-22 11:48:16 +0200

    code polishing after lint check

--- tests/init.pp
+++ tests/init.pp
@@ -1,6 +1,6 @@
 download_uncompress { 'dwnl_inst_libreoffice':
-  download_base_url => "http://jee.invallee.it/dist",
+  download_base_url => 'http://jee.invallee.it/dist',
   dest_folder       => '/tmp',
-  creates           => "/tmp/installFolder",
+  creates           => '/tmp/installFolder',
   uncompress        => 'tar.gz',
 }
